cdcb53fba470fe6b6a7e692be39e925a8b78f70b
Woong Gyu La <juhgiyo@gmail.com>
2015-10-19 00:11:17 +0900

    Minor fix

--- EpServerEngine.cs/EpServerEngine.cs/ClientSide/IOCP/IocpTcpClient.cs
+++ EpServerEngine.cs/EpServerEngine.cs/ClientSide/IOCP/IocpTcpClient.cs
@@ -388,7 +388,19 @@ namespace EpServerEngine.cs
         public void Connect(ClientOps ops)
         {
             if (IsConnectionAlive)
+            {
+                ConnectStatus status = ConnectStatus.FAIL_ALREADY_CONNECTED;
+                if (ops.CallBackObj != null)
+                {
+                    Task t = new Task(delegate()
+                    {
+                        ops.CallBackObj.OnConnected(this, status);
+                    });
+                    t.Start();
+
+                }
                 return;
+            }
             if (ops == null)
                 ops = ClientOps.defaultClientOps;
             if (ops.CallBackObj == null)
